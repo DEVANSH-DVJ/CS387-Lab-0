87ef4b520a450c017033a34cdb0233825d0daf18
devansh-dvj <devansh.dvj@gmail.com>
2022-02-02 23:59:56 +0530

    Restructed testing scripts

--- create_and_insert.py
+++ create_and_insert.py
@@ -1,22 +1,16 @@
 import psycopg2 as pg
 
-with open('PS/DDL.sql', 'r') as file:
-    crt = file.read()
-with open('PS/sampleData.sql', 'r') as file:
-    ins = file.read()
-
 conn = pg.connect(database='Lab0', user='postgres',
                   password='secret', host='localhost', port='5432')
 cur = conn.cursor()
 
-for x in crt.split(';') + ins.split(';'):
-    try:
-        cur.execute(x)
-        conn.commit()
-    except Exception:
-        continue
+with open('PS/DDL.sql', 'r') as file:
+    cur.execute(file.read())
+with open('PS/sampleData.sql', 'r') as file:
+    cur.execute(file.read())
+
+conn.commit()
 
 cur.close()
-
 if (conn):
     conn.close()
--- query.py
+++ query.py
@@ -4,24 +4,16 @@ import psycopg2 as pg
 with open(sys.argv[1], 'r') as file:
     query = file.read()
 
-try:
-    conn = pg.connect(database='Lab0', user='postgres',
-                      password='secret', host='localhost', port='5432')
-    cur = conn.cursor()
+conn = pg.connect(database='Lab0', user='postgres',
+                  password='secret', host='localhost', port='5432')
+cur = conn.cursor()
 
-    cur.execute(query)
+cur.execute(query)
 
-    res = cur.fetchall()
-    for row in res:
-        print(','.join(str(x) for x in row))
+res = cur.fetchall()
+for row in res:
+    print(','.join(str(x) for x in row))
 
-    cur.close()
-
-# Error handling
-except Exception as error:
-    print(error)
-
-# Close the connection
-finally:
-    if (conn):
-        conn.close()
+cur.close()
+if (conn):
+    conn.close()
